7860db6038fc2f0d463e5e0b1a069d945f89a3e3
Vasin Dmitriy <vasindima779@gmail.com>
2014-05-10 09:52:13 +0300

    Added share resource ( with relationships )

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+  namespace :api do
+    resources :shares
+  end
+
 end
