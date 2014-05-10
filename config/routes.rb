a8b743074340713d8a54867f6b958995809c6389
Vasin Dmitriy <vasindima779@gmail.com>
2014-05-10 09:55:06 +0300

    Added Welcome controller/actions for Root/Dashboard displaying

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
+  get '/dashboard' => 'welcome#dashboard'
+
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
